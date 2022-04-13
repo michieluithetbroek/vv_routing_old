77619dd9df9edc6939cbb772b594458e2848c960
Michiel uit het Broek <michieluithetbroek@gmail.com>
2022-04-13 19:24:36 +0200

    add main header

--- vv_routing/main.cpp
+++ vv_routing/main.cpp
@@ -5,10 +5,11 @@
 //  Created by Michiel uit het Broek on 13/04/2022.
 //
 
-#include <iostream>
+#include "main.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
---
+++ vv_routing/main.h
@@ -0,0 +1,13 @@
+//
+//  main.h
+//  vv_routing
+//
+//  Created by Michiel uit het Broek on 13/04/2022.
+//
+
+#ifndef main_h
+#define main_h
+
+#include <iostream>
+
+#endif /* main_h */
