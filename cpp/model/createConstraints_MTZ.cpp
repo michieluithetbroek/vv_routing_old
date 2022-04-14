a459a5ad1459905b0b5e54f40b0af7b3a01ac603
Michiel uit het Broek <michieluithetbroek@gmail.com>
2022-04-15 01:06:11 +0200

    bug MTZ: start route at tile 0 instead of node 0

--- cpp/model/createConstraints_MTZ.cpp
+++ cpp/model/createConstraints_MTZ.cpp
@@ -9,15 +9,20 @@
 
 void Model::createConstraints_MTZ()
 {
-    d_model.addConstr(d_u[0], GRB_EQUAL, 0, "Avoid subtours");
-
+    vector<int> tileSet = d_tileSets[0];
+    
     for (int i = 1; i < d_nNodes; ++i)
     {
         for (int j = 1; j < d_nNodes; ++j)
         {
+            if (inTile(j, tileSet))
+                continue;
+            
             GRBLinExpr expr = (d_u[j] - d_u[i] - 1 + (d_nNodes - 1) * (1 - d_x[i][j]));
             
             d_model.addConstr(expr, GRB_GREATER_EQUAL, 0, "Avoid subtours");
         }
     }
 }
+
+//341080 322456 10602.0875   45    6 13858.6000 10502.2200  24.2%  15.5  100s
