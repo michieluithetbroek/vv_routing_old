1eb07333b33c59ebab369b5530ac1646496384b4
Michiel uit het Broek <michieluithetbroek@gmail.com>
2022-04-29 09:48:57 +0200

    set_nNodes based on tileSets

--- cpp/init/set_nNodes.cpp
+++ cpp/init/set_nNodes.cpp
@@ -2,7 +2,7 @@
 //  set_nNodes.cpp
 //  vv_routing
 //
-//  Loop over edges to find the number of nodes
+// Loop over the tile sets to find the number of nodes
 //
 //  Created by Michiel uit het Broek on 28/04/2022.
 //
@@ -13,8 +13,9 @@ void Init::set_nNodes()
 {
     int maxVal = 0;
     
-    for (Edge const &e: d_edges)
-        maxVal = max(maxVal, max(e.from, e.to));
+    for (auto const &tileSet: d_tileSets_perTile)
+        for (int const idx_node: tileSet)
+          maxVal = max(maxVal, idx_node);
         
     d_nNodes = maxVal + 1;
 }
