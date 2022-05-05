fbe4f4ee98ac8477aeb9bbfd2cef0d342e1f4d14
Michiel uit het Broek <michieluithetbroek@gmail.com>
2022-05-05 21:53:51 +0200

    InitOSM

---
+++ cpp/initOSM/InitOSM.cpp
@@ -0,0 +1,10 @@
+//
+//  InitOSM.cpp
+//  vv_routing
+//
+//  Created by Michiel uit het Broek on 05/05/2022.
+//
+
+#include "InitOSM.ih"
+
+
---
+++ cpp/initOSM/InitOSM.hpp
@@ -0,0 +1,15 @@
+//
+//  InitOSM.hpp
+//  vv_routing
+//
+//  Created by Michiel uit het Broek on 05/05/2022.
+//
+
+#pragma once
+
+#include <stdio.h>
+
+class InitOSM
+{
+    
+};
